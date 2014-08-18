6ff6d25cb4a398d7f8ee699e7e32b5b64f36d811
panyibin <panyibin@outlook.com>
2014-08-18 23:21:33 +0800

    init a new project

---
+++ MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var modelx = new AboutModel();
+
+            modelx.Name = "Yibin Pan";
+
+            modelx.Location = "Beijing";
+
+            modelx.School = "BUPT";
+
+            ViewBag.Locationx = "USA";
+
+            return View(modelx);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
